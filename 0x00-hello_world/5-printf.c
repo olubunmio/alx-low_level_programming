2dea76b10ce48d7bf3d9a29d8bebe9f5ee6cf56d
Olubunmi Onwuemene <olubunmi.onwuemene@gmail.com>
2022-09-09 15:27:18 -0700

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-/** 
- * main - use printf
+/**
+ * main - print using printf
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
