7feb38268863d11c5b685ee1f6d1eb7a52f5af83
Olubunmi Onwuemene <olubunmi.onwuemene@gmail.com>
2022-09-09 15:03:35 -0700

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,11 @@
 
 /** 
  * main - use printf
- * Return 0
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,");
-		return (0);
+	return (0);
 }
