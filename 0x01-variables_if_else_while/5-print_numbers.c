3ecb74baf796aace1bb3e91da51c11ed73940067
Olubunmi Onwuemene <olubunmi.onwuemene@gmail.com>
2022-09-09 20:01:10 -0700

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - single digits
+ *
+ * Return: Always (0) Success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		print(i);
+	}
+	putchar('\n');
+	return (0);
+}
