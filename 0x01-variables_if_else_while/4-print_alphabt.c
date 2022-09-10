d2f1baeeb729ac44cfd3eb0a8c2e6de8a7af43de
Olubunmi Onwuemene <olubunmi.onwuemene@gmail.com>
2022-09-09 19:46:40 -0700

    4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,11 +9,11 @@ int main(void)
 {
 	char i;
 	
-	for (i='a'; i <= 'z'; i++)
+	for (i = 'a'; i <= 'z'; i++)
 	{
 		if (i == 'q' || i == 'e')
 			continue;
-		
+	
 		putchar(i);
 	}
 	putchar ('\n');
