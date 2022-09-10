0ed7b2d38963c1a5a636862c978b4f18adc11794
Olubunmi Onwuemene <olubunmi.onwuemene@gmail.com>
2022-09-09 19:29:39 -0700

    3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet lower and uppercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char l;
+
+	for (l = 'a', l <= 'z', l++)
+	{
+		putchar(l);
+	}
+
+	char u;
+
+	for (u = 'A', u <= 'Z', u++)
+	{
+		putchar(u);
+	}
+
+	putchar('\n');
+	return (0);
+}
