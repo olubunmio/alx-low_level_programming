9fdd277dae9f9a6b7f6c062f6b1e2359854eb373
Olubunmi Onwuemene <olubunmi.onwuemene@gmail.com>
2022-09-13 15:39:34 -0700

    4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -8,7 +8,7 @@
  *
  * Return: return 1 if character is a letter and 0 if character is not a letter
  */
-int isalpha(int c)
+int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
