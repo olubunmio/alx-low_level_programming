ca9ec85873f01a5912c50ea451bc69f009ee5715
Olubunmi Onwuemene <olubunmi.onwuemene@gmail.com>
2022-09-12 21:31:47 -0700

    9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,35 +1,23 @@
 #include <stdio.h>
 
 /**
- * main - digit combinations
+ * main -Entry point
  *
  * Return: Always 0
- *
  */
 int main(void)
 {
-	int n, m, l;
+	int i = '0';
 
-	for (n = 48; n < 58; n++)
+	while (i <= '9')
 	{
-		for (m = 49; m < 58; m++)
+		putchar(i);
+		if (i != '9')
 		{
-			for (l = 50; l < 58; l++)
-			{
-				if (l > m && m > n)
-				{
-					putchar(n);
-					putchar(m);
-					putchar(l);
-					if (n != 55 || m != 56)
-					{
-						putchar(',');
-						putchar(' ');
-
-					}
-				}
-			}
+			putchar(',');
+			putchar(' ');
 		}
+		++1;
 	}
 	putchar('\n');
 	return (0);
