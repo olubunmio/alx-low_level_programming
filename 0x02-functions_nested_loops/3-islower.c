4faf124780fb77a05a4283337fa094d1517ca42b
Olubunmi Onwuemene <olubunmi.onwuemene@gmail.com>
2022-09-13 15:07:23 -0700

    3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - entry point
+ *
+ * Description: checks lowercase character
+ */
+int is_lower(int c)
+{
+	if islower(c)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
