f8c55dbd18274d0e0c8672ac6e16fd4fb775f319
Olubunmi Onwuemene <olubunmi.onwuemene@gmail.com>
2022-09-14 06:41:09 -0700

    8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -17,8 +17,8 @@ void jack_bauer(void)
 			_putchar((i / 10) + '0');
 			_putchar((i % 10) + '0');
 			_putchar(':');
-			_putchar((m / 10) + '0');
-			_putchar((m % 10) + '0');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
 			_putchar('\n');
 		}
 	}
