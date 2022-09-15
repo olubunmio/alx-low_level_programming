ddcba1f0d0ee2fe53e3cc41ac2aca20ad212d79a
Olubunmi Onwuemene <olubunmi.onwuemene@gmail.com>
2022-09-14 20:30:09 -0700

    11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * 
+ * print_to_98 - main entry
+ *
+ * Return: void
+ * @i: number to start counting from
+ */
+void print_to_98(int i)
+{
+	if (i < 98)
+	{
+		for (i = i; i < 98; i++)
+			printf("%d, ", i);
+		printf("%d\n", 98);
+	}
+	else
+	{
+		for (i = i; i > 98; i--)
+			printf("%d, ", i);
+		printf("%d\n", 98);
+	}
+}
