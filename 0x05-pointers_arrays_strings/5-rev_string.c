bdf64195a28edcf6d9d402e1c0b084df0dde64cf
Olubunmi Onwuemene <olubunmi.onwuemene@gmail.com>
2022-09-19 14:53:39 -0700

    5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s - string to be reversed
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int i, j;
+
+	j = strlen(s);
+
+	for (i = j - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+}
