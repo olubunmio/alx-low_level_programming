a06c366405725594050211dbd7ec1cec70b8a193
Olubunmi Onwuemene <olubunmi.onwuemene@gmail.com>
2022-09-19 08:15:40 -0700

    1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * swap_int - swaps values of 2 integers
+ * @a: integer 1
+ * @b: integer 2
+ *
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	b = a;
+	a = b;
+}
