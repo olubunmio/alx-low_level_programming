7710472d5e01ea54329dae12ef267c6c2ad42205
Olubunmi Onwuemene <olubunmi.onwuemene@gmail.com>
2022-09-19 06:35:47 -0700

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - function to reset a variable fro a pointer
+ * @n: character to change
+ *
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+	return (0);
+}
