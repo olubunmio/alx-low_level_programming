3480108a0eb671c1b9276729f53cfceb2c4b85d8
Olubunmi Onwuemene <olubunmi.onwuemene@gmail.com>
2022-09-19 09:02:16 -0700

    4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * print_rev - function to print reverse of a string
+ * @s: character to reverse
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	return (strrev(s));
+}
