70323d48a0ce1185e560f2ae4c0d150397ba6dbb
Olubunmi Onwuemene <olubunmi.onwuemene@gmail.com>
2022-09-19 08:43:35 -0700

    2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+/**
+ * str_len - get lenght of a string
+ * @n: string pointer
+ * Return: integer
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
