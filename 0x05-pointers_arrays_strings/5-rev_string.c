d88dab69e2cea6079c2e8b67dfc6bbe944ef46d6
Olubunmi Onwuemene <olubunmi.onwuemene@gmail.com>
2022-09-20 00:27:14 -0700

    5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,10 +11,11 @@ void rev_string(char *s)
 {
 	int i, l, k;
 	char temp;
+	
 	k = strlen(s);
 	l = strlen(s) - 1;
 	
-	for (i = 0; i < k/2; i++)
+	for (i = 0; i < k / 2; i++)
 	{
 		temp = s[i];
 		s[i] = s[l];
