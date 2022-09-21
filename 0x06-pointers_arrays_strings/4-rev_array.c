9ab7c98a787d41c938cf965846d6e99f6dbb9140
Olubunmi Onwuemene <olubunmi.onwuemene@gmail.com>
2022-09-21 15:09:29 -0700

    4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -12,13 +12,13 @@ void reverse_array(int *a, int n)
 	int temp;
 	int i;
 
-	for (i = 0; i < n / 2; i++)
+	for (i = 0;i < n / 2;i++)
 	{
 		temp = a[i];
 		a[i] = a[n - i -1];
 		a[n - i - 1] = temp;
 	}
-	for (i = 0; i < n; i++)
+	for (i = 0;i < n;i++)
 	{
 		_putchar(a[i]);
 	}
