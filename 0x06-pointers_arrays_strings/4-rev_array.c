82b0b5ada7049eed6b1e2baad3668ace0f7ad3e6
Olubunmi Onwuemene <olubunmi.onwuemene@gmail.com>
2022-09-21 15:07:55 -0700

    4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * reverse_array - function to reverse contents of an array
+ * @a: array to reverse
+ * @n: number of elements in array
+ *
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int temp;
+	int i;
+
+	for (i = 0; i < n / 2; i++)
+	{
+		temp = a[i];
+		a[i] = a[n - i -1];
+		a[n - i - 1] = temp;
+	}
+	for (i = 0; i < n; i++)
+	{
+		_putchar(a[i]);
+	}
+}
