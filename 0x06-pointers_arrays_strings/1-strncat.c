fdeecf78742d356956df0ff9a8f390a8c7b1f1ed
Olubunmi Onwuemene <olubunmi.onwuemene@gmail.com>
2022-09-21 14:28:31 -0700

    1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -14,11 +14,11 @@ char *_strncat(char *dest, char *src, int n)
 	int i, len;
 
 	len = 0;
-	while (dest[len] != '\0')
+	while (dest[i++])
 	{
 		len++;
 	}
-	for (i = 0; src[i] != '\0'; i++, len++)
+	for (i = 0; src[i]; i++)
 	{
 		dest[len] = src[i];
 	}
