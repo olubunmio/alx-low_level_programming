333cd5340d7d6c253eec4c0ffacb84d58a7b2ebc
Olubunmi Onwuemene <olubunmi.onwuemene@gmail.com>
2022-09-21 14:46:41 -0700

    2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,11 +12,12 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int j;
 
-	for (j = 0; src[j] != '\0'; j++)
+	for (j = 0; j < n && src[j] != '\0'; j++)
 	{
 		dest[j] = src[j];
 	}
+	while (j < n)
+		dest[j] = '\0';
 
-	dest[j] = '\0';
 	return (dest);
 }
