d2c98f2bc60551647255760f0aa4b3805dd48bca
Olubunmi Onwuemene <olubunmi.onwuemene@gmail.com>
2022-09-21 14:00:31 -0700

    0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,8 +4,8 @@
 
 /**
  * _strcat - concatenates two strings
- * @*dest: destination string
- * @*src: source string
+ * @dest: destination string
+ * @src: source string
  * Return: char
  */
 char *_strcat(char *dest, char *src)
