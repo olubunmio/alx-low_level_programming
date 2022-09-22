274f4e3198436f43edf8016569e6900768e3b276
Olubunmi Onwuemene <olubunmi.onwuemene@gmail.com>
2022-09-22 05:15:04 -0700

    1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,20 +1,17 @@
 #include "main.h"
-#include <string.h>
-#include <stdio.h>
 
 /**
  * _strncat - concatenates two strings
  * @dest: destination string
  * @src: source string
  * @n: character of bytes
+ *
  * Return: string
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, len;
+	int i = 0, len = 0;
 	
-	i = len = 0;
-
 	while (dest[len] != '\0')
 	{
 		len++;
