adff7cfe5b7906503689eb48495046b1516d81cb
Olubunmi Onwuemene <olubunmi.onwuemene@gmail.com>
2022-09-22 04:39:48 -0700

    6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,23 +3,22 @@
 /**
  * cap_string - capitalize all words of a string
  * @s: string to capitalize
+ *
  * Return: string
  */
 char *cap_string(char *s)
 {
 	int j, k;
 	char a[] = " \t\n,;.!?\"(){}";
-
+	
 	k = 0;
 	while (s[j] != '\0')
 	{
 		if ((s[j] >= 'a' && s[j] <= 'z'))
 		{
 			if (j == 0)
-			{
 				s[j] -= 'a' - 'A';
-			}
-			else 
+			else
 			{
 				for (k = 0; k <= 12; k++)
 				{
