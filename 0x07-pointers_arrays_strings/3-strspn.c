fa7099f3434ed9da4c0ec31a4747d5f1441104cf
Olubunmi Onwuemene <olubunmi.onwuemene@gmail.com>
2022-09-26 14:43:09 -0700

    3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strspn - get lenght of a prefix
+ * @s: string to get
+ * @accept: bytes to return
+ *
+ * Return: int
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i = 0, j = 0; k;
+
+	while (accept(i))
+	{
+		k = 0;
+
+		while (s[k] != 32)
+		{
+			if (accept[i] == s[k])
+			{
+				j++;
+			}
+			k++;
+		}
+		i++;
+	}
+}
