701c8315dc29d57f708ca8061cc0c2d205df33aa
Olubunmi Onwuemene <olubunmi.onwuemene@gmail.com>
2022-09-26 08:55:29 -0700

    1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * _memcpy - function to copy from source to destination
@@ -12,7 +13,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i = 0;
 
-	for (i = 0; src[i] != '\0' && i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		dest[i] = src[i];
 	}
