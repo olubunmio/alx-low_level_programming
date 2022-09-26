5e8d766609363d252a2877173f91355c767082f3
Olubunmi Onwuemene <olubunmi.onwuemene@gmail.com>
2022-09-26 08:46:23 -0700

    1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * _memcpy - function to copy from source to destination
+ * @dest: destination string
+ * @src: source string
+ * @n: number of bytes
+ *
+ * Return: string
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	for (i = 0; src[i] != '\0' && i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
