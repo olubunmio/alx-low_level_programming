12aa9250510329305e3d1f739a4835386d4df7ab
Olubunmi Onwuemene <olubunmi.onwuemene@gmail.com>
2022-09-26 06:14:58 -0700

    0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * _memset - function to fill memory with a constant byte
+ * @s: string to fill
+ * @b: byte to fill
+ * @n: number of bytes of the memory
+ *
+ * Return: string
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int j = 0;
+
+	for (j = 0; j < n; j++)
+	{
+		s[j] = b;
+	}
+	return (s);
+}
