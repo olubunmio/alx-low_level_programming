464d86d8bfaf872ee7095e1407bb8af30ed28f43
Olubunmi Onwuemene <olubunmi.onwuemene@gmail.com>
2022-09-27 14:10:44 -0700

    7

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strpbrk - serach string for any bytes
+ * @s: source string
+ * @accept: characters
+ *
+ * Return: string
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i = 0, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		j = 0;
+
+		while (accept(j))
+		{
+			if (s[i] == accept[j])
+			{
+				s += i;
+				return (s);
+			}
+			j++;
+		}
+	}
+	return ('\0');
+}
