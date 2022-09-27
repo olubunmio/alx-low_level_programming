cfd073c8e95f2178de60c85812c261bc265f1388
Olubunmi Onwuemene <olubunmi.onwuemene@gmail.com>
2022-09-27 15:14:25 -0700

    9

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - set value of pointer to char
+ * @s: string to modify
+ * @to: string to assign
+ *
+ * Return: void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
