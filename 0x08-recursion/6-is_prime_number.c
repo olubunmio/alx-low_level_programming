035178477e0bb9ef03c8669453c4236704ac7538
Olubunmi Onwuemene <olubunmi.onwuemene@gmail.com>
2022-09-29 14:19:58 -0700

    7

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - square root of a number
+ * @n: integer
+ *
+ * Return: integer
+ */
+int is_prime_number(int n)
+{
+	if (n / 1 == n || n / n =1)
+	{
+		return (1);
+	}
+	return (0);
+}
