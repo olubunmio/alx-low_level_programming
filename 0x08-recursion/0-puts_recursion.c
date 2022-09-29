37e63ed1dc5e961e05d4472aaaaffba8b60840af
Olubunmi Onwuemene <olubunmi.onwuemene@gmail.com>
2022-09-29 09:20:09 -0700

    0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string followed by a new lin
+ * @s: string to print
+ *
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	int i = 0;
+
+	if (s[i] != '\0')
+	{
+		return (s[i]);
+	}
+}
