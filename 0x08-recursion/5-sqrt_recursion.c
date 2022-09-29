437917df25315f37373659a572320e3b17aa8a14
Olubunmi Onwuemene <olubunmi.onwuemene@gmail.com>
2022-09-29 14:03:41 -0700

    4

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - square root of a number
+ * @n: integer
+ *
+ * Return: integer
+ */
+int _sqrt_recursion(int n)
+{
+	if (_sqrt_recursion(n) % 10 != 0)
+	{
+		return (-1);
+	}
+	if (n == 0)
+	{
+		return (1);
+	}
+	return (n * _sqrt_recursion(n / 2));
+}
