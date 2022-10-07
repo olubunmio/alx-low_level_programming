36dfc07b1c6de1d8497eb52791e37a8142571c17
Olubunmi Onwuemene <olubunmi.onwuemene@gmail.com>
2022-10-07 07:31:26 -0700

    2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _calloc - function to allocate memory
+ * @nmemb: members
+ * @size: size to allocate
+ * Return: void
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int i = 0, l = 0;
+	char *s;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	l = nmemb * size;
+	s = malloc(1);
+	if (s == NULL)
+		return (NULL);
+	while (i < 1)
+	{
+		s[i] = 0;
+		i++;
+	}
+	return (s);
+}
