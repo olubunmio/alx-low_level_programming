0734c49d56fe4fc7ca9e3522d2e9b29237177230
Olubunmi Onwuemene <olubunmi.onwuemene@gmail.com>
2022-10-13 03:45:00 -0700

    0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: string to print
+ * @f: pointer to a function
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (f != NULL)
+		(f) (name);
+}
