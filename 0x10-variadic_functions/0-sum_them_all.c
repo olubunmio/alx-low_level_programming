46dbd5552e890b8598974a29838caa7f6e8e6d13
Olubunmi Onwuemene <olubunmi.onwuemene@gmail.com>
2022-10-13 13:35:02 -0700

    1

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - function to sum
+ * @n: numbers of integers to sum
+ *
+ * Return: sum of all paramaters
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int c, sum = 0;
+	va_list vaList;
+
+	va_start(vaList, n);
+
+	for (c = 0; c < n; c++)
+	{
+		sum += va_arg(vaList, int);
+	}
+	va_end(vaList);
+	return (sum);
+}
