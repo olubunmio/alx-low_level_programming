54da7f277a18ed89f26833eb19a09924ab1b8886
Olubunmi Onwuemene <olubunmi.onwuemene@gmail.com>
2022-10-25 11:39:26 -0700

    more linked lists

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "list.h"
+#include <stdio.h>
+
+/**
+ * print_listint - prints all the elements of a linked list
+ * @h: pointer
+ *
+ * Return: int
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		nodes++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+
+	return (nodes);
+}
---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include "list.h"
+#include <stdio.h>
+
+/**
+ * listint_len - prints number the elements of a linked list
+ * @h: pointer
+ *
+ * Return: number of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		nodes++;
+		h = h->next;
+	}
+
+	return (nodes);
+}
