f13fd3792721242467768c8c513bc5ba8d16b960
Olubunmi Onwuemene <olubunmi.onwuemene@gmail.com>
2022-10-25 13:10:24 -0700

    2

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - adds a new node
+ * @head: pointer
+ * @n: integer for the new node
+ *
+ * Return: address of the new element
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->next = *head;
+
+	*head = new;
+	
+	return (new);
+}
