b12d245c88bec6a3d10257fefa130b48d71ef1b0
Olubunmi Onwuemene <olubunmi.onwuemene@gmail.com>
2022-10-28 16:24:15 -0700

    3

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * set_bit - set value of bit
+ * @n: bit
+ * @index: index
+ *
+ * Return: int
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+
+	*n ^= (1 << index);
+	
+	return (1);
+}
