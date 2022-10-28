0e43db594d257ae99f828d079085554ec0bed1ff
Olubunmi Onwuemene <olubunmi.onwuemene@gmail.com>
2022-10-28 16:17:27 -0700

    0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,17 +8,20 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0; m = 1;
+	unsigned int num = 0, m = 1;
 	int l;
 
-	if (b == '\0')
+	if (*b == '\0')
 		return (0);
+
 	for (l = 0; b[l];)
 		l++;
+
 	for (l -= 1; l >= 0; l--)
 	{
 		if (b[l] != '0' && b[l] != '1')
 			return (0);
+
 		num += (b[l] - '0') * m;
 		m *= 2;
 	}
