8e1b78c280eb0bc25b34dd1c6dbd5a58f7d18128
Olubunmi Onwuemene <olubunmi.onwuemene@gmail.com>
2022-10-31 13:07:40 -0700

    1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: pointer to the name of the file
+ * @text_content: pointer to a sring to write the file
+ *
+ * Return: int
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int o, w, l = 0;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content != NULL)
+	{
+		for (l = 0; text_content[l];)
+			l++;
+	}
+
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	r = read(o, text_content, l);
+	w = write(STDOUT_FILENO, buffer, r);
+
+	if (o == -1 || w == -1)
+	{
+		return (-1);
+	}
+
+	close(o);
+
+	return (1);
+}
